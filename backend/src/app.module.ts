a5cff292735c02a338df58d7c5e22c4abac3a205
deltam3 <practicect@proton.me>
2024-10-27 11:22:05 +0900

    instantiated auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+// import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    AuthModule,
+    // UserModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'mysql_db',
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+// import { AuthService } from './auth.service';
+
+@Module({
+  // imports: [],
+  // controllers: [AuthController],
+  // providers: [AuthService],
+})
+export class AuthModule {}
