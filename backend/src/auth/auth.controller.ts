1a691535f79e8b21198f590ef252ea1439528a23
deltam3 <practicect@proton.me>
2024-10-29 10:50:10 +0900

    npm i class-validator class-transformer

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
+  signup(@Body('email') email: string, @Body('password') password: string) {
     return this.authService.signup();
   }
 
